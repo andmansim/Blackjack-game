14be166368b977e7a628fd82fc32d107fe663bb2
andmansim <91722324+andmansim@users.noreply.github.com>
2021-11-18 19:49:00 +0100

    rules

---
+++ play.py
@@ -0,0 +1,13 @@
+# How to play blackjack?:
+# player wins with 21 points or a higher value than the Dealer = distribuidor, without going bust = have a number higer than 21. You can win if the Dealer Busts and the player not. 
+# The player see the first card of the Dealer. 
+# movements: Hit = ask for more cards, but the maximun is 4. 
+#           Stand = when you decide not to continue. But the Dealer will continue getting cards until it Bust, Stand or Win.  
+#           Bust = player automatically lose if they get Bust, it doesn't matter if the Dealer does too.     
+#           Push = when the player and the Dealer have blackjack. It return the best = apuesta and the cards are deal = repartir, again
+#           Doubling Down = you can doit if you are dealt with a strong hand, and you can get a card before showing it
+#           Splitting a pair = you can doit if the fist dealt the two cards are the same one. So you will play with two hands at the same time
+#           Insurance = seguro, if the exposed card of the Deal is an Ace, you can do a bet to insurance, this is independent of the inicial one, it's 2:1
+# winning the game: if the player wins, it's bet will return and you recieve the equal number of chips from the Dealer
+# If you win with a Blackjack = 21 points, your bet will return and you recieve 3 chips for every 2 chips that you bet
+# cards: 2 decks of 52 each one. The cards are from 2 to 10 with their equal value, Q = J = K =10, and As = 1/11
